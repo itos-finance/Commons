c5fca354d2bac884b1ad15d2b153b5ffa66b73bd
Terence An <terence@itos.fi>
2023-04-28 22:21:17 -0400

    Moving create2 determinism functionality into ContractLib with tests

--- lib/Util/Contract.sol
+++ lib/Util/Contract.sol
@@ -46,7 +46,7 @@ library ContractLib {
                     )
                 )
             )
-        )
+        );
     }
 
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -6,6 +6,7 @@ import { PRBTest } from "@prb/test/PRBTest.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
 
 import { ContractLib } from "Util/Contract.sol";
+import { MockFactory, MockPool } from "TestLib/MockPool.u.sol";
 
 contract ContractLibTest is PRBTest, StdCheats {
     function testIsContract() public {
@@ -16,4 +17,14 @@ contract ContractLibTest is PRBTest, StdCheats {
         vm.expectRevert(ContractLib.NotAContract.selector);
         ContractLib.assertContract(address(0));
     }
+
+    function testGetCreate2Address() public {
+        MockFactory factory = new MockFactory();
+        bytes32 salt = bytes32(uint256(0));
+        address pool = factory.createPool(salt);
+        bytes32 initCodeHash = keccak256(type(MockPool).creationCode);
+        address createAddr = ContractLib.getCreate2Address(address(factory), salt, initCodeHash);
+        assertEq(pool, createAddr);
+    }
+
 }
