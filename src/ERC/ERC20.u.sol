30da10198396defb95f00a823d42fff973fe2f96
Terence An <terence@itos.fi>
2023-09-12 11:51:33 -0400

    Remove ownership requirement in MintableERC20. Add OwnedMintableERC20

--- src/ERC/ERC20.u.sol
+++ src/ERC/ERC20.u.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.13;
 import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MintableERC20 is ERC20 {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+}
+
+contract OwnedMintableERC20 is ERC20 {
     address public owner;
 
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
