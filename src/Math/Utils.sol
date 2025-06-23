097ce088eccf020325f5437595a9cb754a98c6c9
Terence An <terence@itos.fi>
2025-06-23 14:59:20 -0400

    Switch to stable build and see if the custom errors in test work

--- src/Math/Utils.sol
+++ src/Math/Utils.sol
@@ -112,10 +112,10 @@ library MathUtils {
     /// This rounds down. Generally, you'll want to multiply this ratio with another value through X256.mul256.
     /// @dev b must be greater than 1.
     /// @dev BE AWARE OF WHEN THIS IS INACCURATE. THERE ARE VERY RARE INSTANCES WHERE THIS IS APPROPRIATE.
-    /// The inaccuracy is significant.
+    /// The inaccuracy is significant. DO NOT USE WHERE PRECISION IS REQUIRED. Consider mulDivX256 instead.
     /// @custom:gas 104
     function percentX256(uint256 a, uint256 b) internal pure returns (uint256 ratioX256) {
-        if (a == b) return uint256(int256(-1));
+        if (a == b) return type(uint256).max; // 100% is the max ratio.
         /// We actually compute 2^256 / b first extremely cheaply. ~20 gas
         require(b > 1, "0");
         assembly {
